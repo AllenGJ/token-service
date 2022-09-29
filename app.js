68c8a005dad820c4db583cc8ceaed526b6dc8c4c
Allen G J <allengj77@gmail.com>
2022-09-29 15:07:27 +0530

    Use env variable for port

--- app.js
+++ app.js
@@ -4,8 +4,8 @@ const app = express();
 import {expressLoader} from "./src/loaders/express.js";
 expressLoader(app);
 
-const server = app.listen(3000, () => {
-    console.log("Express server running on port 3000");
+const server = app.listen(process.env.PORT, () => {
+    console.log("Express server running on port", process.env.PORT);
 });
 
 export default server;
